05f0f23e65a861a0e1c762a14381d695278c8718
Karan Chandekar <karanchandekar431@gmail.com>
2022-04-22 21:02:21 +0530

    create some variables

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 const textEl = document.getElementById("text");
 const speedEl = document.getElementById("speed");
+const text = "We Love Programming!";
+let idx = 1;
+let speed = 300 / speedEl.value;
